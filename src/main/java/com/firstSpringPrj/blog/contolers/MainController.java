d00fabfadbac1d730bf11ee59e1350b796ce517d
sergey <wendingnn@gmail.com>
2025-03-31 14:26:40 +0300

    MainContoller

---
+++ src/main/java/com/firstSpringPrj/blog/contolers/MainController.java
@@ -0,0 +1,17 @@
+package com.firstSpringPrj.blog.contolers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "main page");
+        return "home";
+    }
+
+}
