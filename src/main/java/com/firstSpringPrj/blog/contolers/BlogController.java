46a97bdf9cfbbd74cad8ae41e1ec36d9883c7f6e
sergey <wendingnn@gmail.com>
2025-03-31 18:31:12 +0300

    create 'BlogController.java'

---
+++ src/main/java/com/firstSpringPrj/blog/contolers/BlogController.java
@@ -0,0 +1,13 @@
+package com.firstSpringPrj.blog.contolers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        return "blog-main";
+    }
+}
